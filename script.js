2bed8f6bbbb074d4695576e7ecd74de13faae437
Graham Hall <ghall@grahams-macbook-air.local>
2022-06-08 23:59:39 +0000

    basic countdown logic

--- script.js
+++ script.js
@@ -2,4 +2,36 @@ const instructEl = document.querySelector('#instruction');
 const countdownEl = document.querySelector('#countdown');
 const startBtn = document.querySelector('#start');
 
-const exercise = () => {};
+const countdown = num => {
+	countdownEl.innerText = num;
+	let nextNum;
+
+	switch (num) {
+		case 4:
+			instructEl.innerText = 'Breathe in.';
+			nextNum = 7;
+			break;
+		case 7:
+			instructEl.innerText = 'Hold.';
+			nextNum = 8;
+			break;
+		case 8:
+			instructEl.innerText = 'Breathe out.';
+			break;
+	}
+
+	const interval = setInterval(() => {
+		num--;
+		countdownEl.innerText = num;
+		if (nextNum && num === 0) {
+			clearInterval(interval);
+			countdown(nextNum);
+		} else if (!nextNum && num === 0) {
+			clearInterval(interval);
+		}
+	}, 1000);
+};
+
+const exercise = () => {
+	countdown(4);
+};
