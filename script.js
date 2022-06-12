cf82c8aac3aa1674cc0c352ae526177d186bef4d
Graham Hall <ghall@grahams-macbook-air.local>
2022-06-12 17:40:15 +0000

    animation test

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const instructEl = document.querySelector('#instruction');
-const countdownEl = document.querySelector('#countdown');
 const startBtn = document.querySelector('#start');
 
 let count = 5;
 
 const countdown = num => {
+	const instructEl = document.querySelector('#instruction');
+	const countdownEl = document.querySelector('#countdown');
+	const circleEl = document.querySelector('#circle');
+
 	countdownEl.innerText = num;
 	let nextNum;
 
 	switch (num) {
 		case 4:
 			instructEl.innerText = 'Breathe in.';
+			circleEl.className = 'inhale';
 			nextNum = 7;
 			break;
 		case 7:
 			instructEl.innerText = 'Hold.';
+			circleEl.className = 'hold';
 			nextNum = 8;
 			break;
 		case 8:
 			instructEl.innerText = 'Breathe out.';
+			circleEl.className = 'exhale';
 			break;
 	}
 
